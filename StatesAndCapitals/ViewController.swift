3d2ac2b5634f08c44518507aa582e30c053bd2ba
James Chun <jameswchun@gmail.com>
2018-11-06 20:26:37 -0500

    Initial Commit

---
+++ StatesAndCapitals/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StatesAndCapitals
+//
+//  Created by James Chun on 11/6/18.
+//  Copyright © 2018 James Chun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ StatesAndCapitalsTests/StatesAndCapitalsTests.swift
@@ -0,0 +1,34 @@
+//
+//  StatesAndCapitalsTests.swift
+//  StatesAndCapitalsTests
+//
+//  Created by James Chun on 11/6/18.
+//  Copyright © 2018 James Chun. All rights reserved.
+//
+
+import XCTest
+@testable import StatesAndCapitals
+
+class StatesAndCapitalsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
